b2d914f05d7fd4f9fb18a68a6b0c81f1c72d1319
Jonas Nockert <jonasnockert@gmail.com>
2014-04-24 22:29:14 +0200

    Added Mideye login/otp pages.

---
+++ js/login.js
@@ -0,0 +1,26 @@
+/*jslint browser: true, vars: true*/
+/*global $, jQuery*/
+
+/*
+ * ksuonline.se
+ *
+ * Kärnkraftsäkerhet och Utbildning AB 2014
+ *
+ */
+
+$(function () {
+    'use strict';
+
+    // Slight delay before starting scale animation.
+    setTimeout(function () {
+        $('body').addClass('loaded');
+    }, 300);
+
+    // Initialize starfield parallax scrolling.
+    $('#parallax').parallax({limitY: 0});
+
+    $('form').submit(function () {
+        $('form :input').prop('readonly', true);
+        $('form :submit').prop('disabled', true);
+    });
+});
