7c4559dddc9a602d655e7662c30991db2b3d6caf
Jonas Nockert <jonasnockert@gmail.com>
2014-04-29 14:26:55 +0200

    Friendlier mideye messages.

--- js/login.js
+++ js/login.js
@@ -23,6 +23,25 @@ $(function () {
     // aren't trying hard enough yet.
     $('input').placeholder();
 
+    // Replace common radius info/error messages.
+    var msg = $('.err').text().trim();
+    var found;
+    if (msg === 'Enter OTP:') {
+        $('.err').text('');
+    } else if (msg === 'Invalid username or password') {
+        $('.err').text('Ogiltigt användarnamn eller lösenord.');
+    } else if (msg === 'Invalid OTP.') {
+        $('.err').text('Ogiltig engångskod. Var vänlig försök igen.');
+    } else {
+        found = msg.match(/^Phone not reachable\. Please sign (\d+)\.$/);
+        if (found) {
+            $('.err').html(['Din mobil kunde inte nås. Om du har appen',
+                            'Mideye+ installerad kan du öppna den och',
+                            'signera med',
+                            '<code>' + found[1] + '</code>.'].join(' '));
+        }
+    }
+
     $('form').submit(function () {
         $('form :input').prop('readonly', true);
         $('form :submit').prop('disabled', true);
